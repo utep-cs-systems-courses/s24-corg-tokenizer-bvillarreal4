e1bcd1252ba51943efbb8200f95b7bafc6480097
bvillarreal4 <bvillarreal4@miners.utep.edu>
2024-04-04 16:10:09 -0600

    worked on main

--- src/uimain.c
+++ src/uimain.c
@@ -1,10 +1,59 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "stdlib.h"
+#include "tokenizer.h"
+#include "history.h"
 
 int main(){
-  fputs("Enter input: \n>" , stdout);
-
   char input[100];
-  scanf("%[^\n]s", input);
-  printf("%s\n", input);
+  char *str;
+  char **tokens = (char** )malloc(100 * sizeof(input));
+  List *history = init_history();
+
+  goto main;
+
+ main:
+  printf("Enter your input. If done inputting, enter an empty input \n");
+  printf("Enter '!' + the ID of an input you want to view \n");
+  printf("Enter 'h' to view the entire history \n");
+
+  do{
+    printf("> ");
+    int id;
+    fgets(input, sizeof(input), stdin);
+    str = input;
+
+    //checks if the input is empty indicating the user is done inputting lines
+    if(input[0] == '\n'){
+      break;
+    }
+
+    //switch cases for user input, if the user inputs ! it will print the id otherwise it will
+    //add the users inputted string into the history
+
+    switch(input[0]){
+
+    case '!':
+      //converts the character after the ! into a number and assigns it to the id value
+      input[0] = ' ';
+      id = atoi(input);
+      printf(get_history(history,id));
+      break;
+
+      //case that prints entire history
+    case 'h':
+      print_history(history);
+  
+      break;
+      
+    //default case that adds the users input to the history list
+    default:
+      tokens = tokenize(str);
+
+      for(int i = 0; i < count_tokens(str); i++){
+	add_history(history, tokens[i]);
+      }
+      break;
+    }
+  }while(input[0] != '\n');
 
 }
